f07eba1c53ce776c5d25cf61d54864ea6ea94515
Mike Yu <mikeyu@Mikes-Mac-mini.lan>
2024-06-17 22:18:23 -0700

    added swipe feature

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -1,38 +1,61 @@
-import { Text, View , Image, StyleSheet, ScrollView} from 'react-native';
+import { Text, View , Image, StyleSheet, ScrollView, Button} from 'react-native';
 import { Link } from 'expo-router';
 import data from "../../assets/data/demo"
 import { SafeAreaView } from 'react-native-safe-area-context';
-
+import Swiper from 'react-native-deck-swiper'
 
 
 
 export default function Page() {
     return (
-    <ScrollView style={{height: 10}}>
-<View>
-        {data.map((item) => (
-          <View key={item.id} style={styles.card}>
-            <Text>{item.name}</Text>
-            <Image source={item.image} style={styles.image} />
-            <Text>{item.description}</Text>
-            <Text>{item.message}</Text>
-          </View>
-        ))}
-      </View>
-    </ScrollView>
-      
+        <View>
+            <Swiper
+            style= {styles.container}
+                cards={data}
+                renderCard={(card) => {
+                    return (
+                        <View key={card.id} style={styles.card}>
+                            <Text>{card.name}</Text>
+                            <Image source={card.image} style={styles.image} />
+                            <Text>{card.description}</Text>
+                            <Text>{card.message}</Text>
+                        </View>
+                    )
+                }}
+                onSwiped={(cardIndex) => {console.log(cardIndex)}}
+                onSwipedAll={() => {console.log('onSwipedAll')}}
+                cardIndex={0}
+                backgroundColor={'#4FD0E9'}
+                stackSize= {3}>
+               
+            </Swiper>
+        </View>
     );
-  }
+}
   
-  const styles = StyleSheet.create({
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      
+    
+    },
     card: {
-      borderWidth: 1,
-      borderColor: '#000',
-      padding: 10,
-      marginBottom: 10,
+      flex: 1,
+      borderRadius: 4,
+      borderWidth: 2,
+      borderColor: "#E8E8E8",
+      justifyContent: "center",
+      backgroundColor: "white"
+    },
+    text: {
+      textAlign: "center",
+      fontSize: 50,
+      backgroundColor: "transparent"
     },
     image: {
-      width: 100,
-      height: 100,
+        width: 100,
+        height: 100,
+        resizeMode: 'contain',
+        marginBottom: 20,
     },
   });
